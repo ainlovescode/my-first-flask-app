e17f1799722c54056ac4731e0fd500dbb6341145
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2020-10-28 02:39:01 +0800

    Update to parse signup form data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,19 @@ app = Flask(__name__)
 def hello():
     # return 'Hello, World!'
     return render_template("home.html")
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+
+    if request.method == "POST":
+
+        req = request.form
+        # Uncomment line below to test form data is passed correctly
+        # print("Username is", req.get("username"))
+        return redirect(request.url)
+
+    return render_template("signup.html")
+
 
     
 if __name__ == "__main__":
