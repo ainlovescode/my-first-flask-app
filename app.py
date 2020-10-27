91e6cbf8b26ea39e6bb8ff173f783fc0e977470f
Ainul-MdRazib <ainul_mardhiyyah@mymail.sutd.edu.sg>
2020-10-28 03:34:35 +0800

    Redirect valid sign up to homepage without data storage

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
 
+app.secret_key=b'1jb@9^$'
+
 @app.route('/')
-def hello():
+@app.route('/home')
+def home():
     # return 'Hello, World!'
     return render_template("home.html")
 
@@ -13,9 +16,15 @@ def sign_up():
     if request.method == "POST": # POST "hides" data in request instead of pass to URL in GET method
 
         # Uncomment the two line below to test form data is passed correctly
-        # req = request.form
-        # print("Username is", req.get("username"))
-        return redirect(request.url)
+        request_data = request.form
+        sugg_username = request_data.get("username")
+        sugg_password = request_data.get("password")
+
+        if sugg_username != "" and sugg_password != "":
+            flash("Sign up accepted. Try logging in!")
+            return redirect("home")
+        else:
+            return redirect(request.url)
 
     return render_template("signup.html")
 
